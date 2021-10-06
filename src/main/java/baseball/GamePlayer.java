417042edc18e39d9484629e8bd14515d72ee4526
DESKTOP-PGNRMR7\sunju <sunju635@naver.com>
2021-10-06 19:30:10 +0900

    refactor($test): delete test code

--- src/main/java/baseball/GamePlayer.java
+++ src/main/java/baseball/GamePlayer.java
@@ -19,9 +19,6 @@ public class GamePlayer {
     }
 
     public GameResults play(String numbers) throws GameException {
-        /*TODO
-        *  테스트를 위한 로그 코드로 삭제해야함*/
-        System.out.println(gameNumbers);
         GameResults gameResults = gameNumbers.attack(new AttackNumbers(numbers));
         gameResults.sort(); // GameResult 순서대로 출력되도록 sorting을 해준다.
         return gameResults;
